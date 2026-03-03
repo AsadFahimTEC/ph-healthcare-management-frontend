0f176404f276bfbb5c70c4b45249099f33a35220
AsadFahimTEC <fah485434@gmail.com>
2026-03-03 16:12:41 +0600

    setting new access token in frontend using refresh token inside proxy file

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,20 +1,21 @@
-// async function refreshTokenMiddleware (refreshToken : string) : Promise<boolean> {
-//     try {
-//         const refresh = await getNewTokensWithRefreshToken(refreshToken);
-//         if(!refresh){
-//             return false;
-//         }
-//         return true;
-//     } catch (error) {
-//         console.error("Error refreshing token in middleware:", error);
-//         return false;   
-//     }
-// }
-
 import { NextRequest, NextResponse } from "next/server";
 import { jwtUtils } from "./lib/jwtUtils";
 import { getDefaultDashboardRoute, getRouteOwner, isAuthRoute, UserRole } from "./lib/authUtils";
+import { getNewTokensWithRefreshToken } from "./services/auth.service";
+import { isTokenExpiringSoon } from "./lib/tokenUtils";
 
+async function refreshTokenMiddleware (refreshToken : string) : Promise<boolean> {
+    try {
+        const refresh = await getNewTokensWithRefreshToken(refreshToken);
+        if(!refresh){
+            return false;
+        }
+        return true;
+    } catch (error) {
+        console.error("Error refreshing token in middleware:", error);
+        return false;   
+    }
+}
 
 export async function proxy (request : NextRequest) {
    try {
@@ -42,39 +43,39 @@ export async function proxy (request : NextRequest) {
 
 
        //proactively refresh token if refresh token exists and access token is expired or about to expire
-    //    if (isValidAccessToken && refreshToken && (await isTokenExpiringSoon(accessToken))){
-    //         const requestHeaders = new Headers(request.headers);
+       if (isValidAccessToken && refreshToken && (await isTokenExpiringSoon(accessToken))){
+            const requestHeaders = new Headers(request.headers);
 
-    //         const response = NextResponse.next({
-    //             request: {
-    //                 headers : requestHeaders
+            const response = NextResponse.next({
+                request: {
+                    headers : requestHeaders
             
-    //             },
-    //         })
+                },
+            })
 
 
-    //         try {
-    //             const refreshed = await refreshTokenMiddleware(refreshToken);
+            try {
+                const refreshed = await refreshTokenMiddleware(refreshToken);
 
-    //             if(refreshed){
-    //                 requestHeaders.set("x-token-refreshed", "1");
-    //             }
+                if(refreshed){
+                    requestHeaders.set("x-token-refreshed", "1");
+                }
 
-    //             return NextResponse.next(
-    //                 {
-    //                     request: {
-    //                         headers : requestHeaders
-    //                     },
-    //                     headers : response.headers
-    //                 }
-    //             )
-    //         } catch (error) {
-    //             console.error("Error refreshing token:", error);
+                return NextResponse.next(
+                    {
+                        request: {
+                            headers : requestHeaders
+                        },
+                        headers : response.headers
+                    }
+                )
+            } catch (error) {
+                console.error("Error refreshing token:", error);
 
-    //         }
+            }
 
-    //         return response;
-    //    }
+            return response;
+       }
 
 
        // Rule - 1 : User is logged in (has access token) and trying to access auth route -> allow
