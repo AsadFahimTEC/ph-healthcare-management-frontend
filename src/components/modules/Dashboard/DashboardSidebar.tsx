bd0c73312dd38fd7fd0a3f1d76337d05bc355db4
AsadFahimTEC <fah485434@gmail.com>
2026-03-06 14:11:55 +0600

    creating dynamic role based navigation items in sidebar

--- src/components/modules/Dashboard/DashboardSidebar.tsx
+++ src/components/modules/Dashboard/DashboardSidebar.tsx
@@ -1,6 +1,14 @@
+import { getDefaultDashboardRoute } from "@/lib/authUtils";
+import { getNavItemsByRole } from "@/lib/axios/navItems";
+import { getUserInfo } from "@/services/auth.service";
+import { NavSection } from "@/types/dashboard.types";
 
 
-const DashboardSidebar = () => {
+const DashboardSidebar = async () => {
+    const userInfo = await getUserInfo()
+    const navItems: NavSection[] = getNavItemsByRole(userInfo.role)
+
+    const dashboardHome = getDefaultDashboardRoute(userInfo.role)
     return (
         <div>
             DashboardSidebar
