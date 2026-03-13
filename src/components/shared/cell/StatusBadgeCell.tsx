d645ca2a495f9ae45b79ec5cd629a4a94e00a645
AsadFahimTEC <fah485434@gmail.com>
2026-03-13 16:10:14 +0600

    creating status badge cell component for table

--- src/components/shared/cell/StatusBadgeCell.tsx
+++ src/components/shared/cell/StatusBadgeCell.tsx
@@ -1,20 +1,20 @@
-// import { Badge } from "@/components/ui/badge";
-// import { UserStatus } from "@/types/doctor.types";
+import { Badge } from "@/components/ui/badge";
+import { UserStatus } from "@/types/doctor.types";
 
-// interface IStatusBadgeCellProps {
-//     status : UserStatus;
-// }
+interface IStatusBadgeCellProps {
+    status : UserStatus;
+}
 
-// const StatusBadgeCell = ({ status }: IStatusBadgeCellProps) => {
-//   return (
-//     <Badge
+const StatusBadgeCell = ({ status }: IStatusBadgeCellProps) => {
+  return (
+    <Badge
     
-//         variant={status === UserStatus.ACTIVE ? "default" : status === UserStatus.BLOCKED ? "destructive" : "secondary"}
-//         // className="px-2 py-1"
-//     >
-//         <span className="text-sm capitalize">{status.toLowerCase()}</span>
-//     </Badge>
-//   )
-// }
+        variant={status === UserStatus.ACTIVE ? "default" : status === UserStatus.BLOCKED ? "destructive" : "secondary"}
+        // className="px-2 py-1"
+    >
+        <span className="text-sm capitalize">{status.toLowerCase()}</span>
+    </Badge>
+  )
+}
 
-// export default StatusBadgeCell
+export default StatusBadgeCell
