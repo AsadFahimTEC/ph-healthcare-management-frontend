94f567e05cc4c48caaea49cc486f4f44c1a31e9b
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2026-04-28 06:01:26 +0600

    making the homepage with ai assistant

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,10 +1,20 @@
-import { Button } from "@/components/ui/button";
+import Hero from "@/components/modules/Home/Hero";
+import PublicFooter from "@/components/modules/Home/PublicFooter";
+import PublicNavbar from "@/components/modules/Home/PublicNavbar";
+import Steps from "@/components/modules/Home/Steps";
+import TopDoctors from "@/components/modules/Home/TopDoctors";
+
 
 export default function Home() {
   return (
-    <div>
-        Common
-      <Button variant="outline">Hello World</Button>
+    <div className="min-h-screen flex flex-col">
+      <PublicNavbar />
+      <main className="flex-grow">
+        <Hero />
+        <Steps />
+        <TopDoctors />
+      </main>
+      <PublicFooter />
     </div>
   );
 }
